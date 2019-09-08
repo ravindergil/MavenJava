2dc5d7efbc51956cfeab1590a2832e41f3bc735c
Ravinder Gill <ravigill@in.ibm.com>
2019-09-09 00:12:39 +0530

    first commit

---
+++ src/test/java/MyProject/RESTAssuredTest.java
@@ -0,0 +1,17 @@
+package MyProject;
+
+import org.testng.annotations.Test;
+
+public class RESTAssuredTest {
+	
+	@Test
+	public void GETPost() {
+		System.out.println("GETPost");
+	}
+	
+	@Test
+	public void DELETEPost() {
+		System.out.println("DELETEPost");
+	}
+
+}
---
+++ src/test/java/MyProject/SeleniumTest.java
@@ -0,0 +1,16 @@
+package MyProject;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	public void BrowserAutomation() {
+		System.out.println("BrowserAutomation");
+	}
+	
+	@Test
+	public void ElementUI() {
+		System.out.println("ElementUI");
+	}
+}
